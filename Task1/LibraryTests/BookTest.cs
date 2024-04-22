9924bc77dcf8f5b773310a48eb292a5fd561cbf5
Marcin Grzelak <mgrzelak218@gmail.com>
2024-04-22 22:14:05 +0200

    first test

---
+++ Task1/LibraryTests/BookTest.cs
@@ -0,0 +1,62 @@
+using NUnit.Framework;
+using System;
+using Library;
+
+namespace LibraryTests
+{
+    [TestFixture]
+    public class BookTests
+    {
+        [Test]
+        public void Book_Constructor_WithValidParameters_ShouldCreateBook()
+        {
+            // Arrange
+            int bookId = 1;
+            string title = "The Great Gatsby";
+            string author = "F. Scott Fitzgerald";
+            int term = 30;
+
+            // Act
+            Book book = new Book(bookId, title, author, term);
+
+            // Assert
+            Assert.AreEqual(bookId, book.GetId());
+            Assert.AreEqual(title, book.GetTitle());
+            Assert.AreEqual(author, book.GetAuthor());
+            Assert.AreEqual(term, book.GetTerm());
+        }
+
+        [Test]
+        public void SetTerm_WithValidTerm_ShouldUpdateTerm()
+        {
+            // Arrange
+            int bookId = 1;
+            string title = "The Great Gatsby";
+            string author = "F. Scott Fitzgerald";
+            int term = 30;
+            Book book = new Book(bookId, title, author, term);
+
+            // Act
+            int newTerm = 60;
+            book.SetTerm(newTerm);
+
+            // Assert
+            Assert.AreEqual(newTerm, book.GetTerm());
+        }
+
+        [Test]
+        public void SetTerm_WithInvalidTerm_ShouldThrowArgumentException()
+        {
+            // Arrange
+            int bookId = 1;
+            string title = "The Great Gatsby";
+            string author = "F. Scott Fitzgerald";
+            int term = 30;
+            Book book = new Book(bookId, title, author, term);
+
+            // Act and Assert
+            Assert.That(() => book.SetTerm(0), Throws.ArgumentException);
+            Assert.That(() => book.SetTerm(-1), Throws.ArgumentException);
+        }
+    }
+}
