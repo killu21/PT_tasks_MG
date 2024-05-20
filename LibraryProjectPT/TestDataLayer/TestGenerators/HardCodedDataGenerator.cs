b3c484c726fe799787d7b98f1bb5292845e757f2
IK <i.kawczynskiformal@gmail.com>
2024-05-20 17:17:57 +0200

    All Data Layer unit tests pass

--- LibraryProjectPT/TestDataLayer/TestGenerators/HardCodedDataGenerator.cs
+++ LibraryProjectPT/TestDataLayer/TestGenerators/HardCodedDataGenerator.cs
@@ -27,12 +27,12 @@ internal class HardCodedDataGenerator : IDataGenerator
     {
         List<User> users = new()
         {
-            new Customer(1, "Alice", "Smith", 123456789),
-            new Customer(2, "Bob", "Johnson", 345678901),
-            new Customer(3, "Charlie", "Brown", 567890123),
-            new Staff(1001, "David", "Miller", 678901234),
-            new Staff(1002, "Eva", "Green", 789012345),
-            new Staff(1003, "Frank", "Sinatra", 890123456), 
+            new Customer(1, "Smith", "Alice", 123456789),
+            new Customer(2, "Johnson", "Bob", 345678901),
+            new Customer(3, "Brown", "Charlie", 567890123),
+            new Staff(1001, "Miller", "David", 678901234),
+            new Staff(1002, "Green", "Eva", 789012345),
+            new Staff(1003, "Sinatra", "Frank", 890123456), 
         };
 
         foreach (User user in users)
